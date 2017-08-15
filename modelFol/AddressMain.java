1ab203072ea856bc66062fc3d16d680ac1c1362f
Miracle Okafor <30794063+devMiracle@users.noreply.github.com>
2017-08-15 18:44:16 +0100

    This is our Main class

---
+++ modelFol/AddressMain.java
@@ -0,0 +1,21 @@
+/*
+ * Started to Build this application on the
+ *      3rd of August 2017
+ */
+package address.model;
+
+import address.view.AddressAppView;
+
+/**
+ * @author Miracle Chukwuemeka Okafor
+ */
+public class AddressMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AddressAppView address = new AddressAppView();
+        
+        address.setVisible(true);
+        address.setLocationRelativeTo(null);
+    }
+}
